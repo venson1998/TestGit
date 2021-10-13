157d8a52e5de33f1a42a53693617821ff30f0e1c
vv <1366237319@qq.com>
2021-10-13 14:06:01 +0800

    second commit

---
+++ src/main/java/com/vv/GTest.java
@@ -0,0 +1,10 @@
+package com.vv;
+
+public class GTest {
+    public static void main(String[] args) {
+        System.out.println("sss1");
+        System.out.println("sss2");
+        System.out.println("sss3");
+
+    }
+}
